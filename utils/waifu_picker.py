8e396958b4c0a8f06a8b7026b9908015d0a8b753
KIMGITACHI0916 <148501410+KIMGITACHI0916@users.noreply.github.com>
2025-09-06 22:46:10 +0530

    Update waifu_picker.py

--- utils/waifu_picker.py
+++ utils/waifu_picker.py
@@ -1,8 +1,11 @@
 import json
 import random
+import os
+
+WAIFU_FILE = os.path.join(os.path.dirname(__file__), "../waifu_data/waifus.json")
 
 def load_waifus():
-    with open("waifu_data/waifus.json", "r") as f:
+    with open(WAIFU_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 def pick_random_waifu():
